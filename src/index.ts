626dfef4773c2d487246597e2f84ed8ff86b972e
davgarciaco <david@davidgarcia.co>
2022-01-04 21:57:07 -0500

    gui w/k value input, added divorce and retirment party data

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,19 @@ const weddingValues = data.filter(d => d.type == EVENT_TYPES[1]).map(d => ({
     x: d.guests,
     y: new Date(d.date).getHours()
 }));
+const divorceValues = data.filter(d => d.type == EVENT_TYPES[2]).map(d => ({
+    x: d.guests,
+    y: new Date(d.date).getHours()
+}));
+const retirementValues = data.filter(d => d.type == EVENT_TYPES[3]).map(d => ({
+    x: d.guests,
+    y: new Date(d.date).getHours()
+}));
 //Create Scatter Plot
 const init = async () => {
-    tfvis.render.table({name: 'Data', tab: 'Data'}, {
-        headers: ['Time','Guests' , 'Type'],
-        values: data.map( (d: Party) => [d.date, d.guests,d.type])
+    tfvis.render.table({ name: 'Data', tab: 'Data' }, {
+        headers: ['Time', 'Guests', 'Type'],
+        values: data.map((d: Party) => [d.date, d.guests, d.type])
     })
 }
 /* UNUSED
@@ -38,35 +46,45 @@ const init = async () => {
 
 // tfvis.show.modelSummary({ name: 'Model Summary' }, MODEL);
 */
-tfvis.show.valuesDistribution({ name: 'Distribution: Hours' }, tf.tensor1d(data.map( (d: Party) => {
+tfvis.show.valuesDistribution({ name: 'Distribution: Hours' }, tf.tensor1d(data.map((d: Party) => {
     let { date } = d;
     let time = new Date(date).getHours() ? new Date(date).getHours() : null;
     return time;
 })));
-tfvis.render.scatterplot({name: 'Event Data', 'tab': 'ScatterPlot'}, {
-    values: [birthdayValues, weddingValues],
-    series: [EVENT_TYPES[0], EVENT_TYPES[1]]
+tfvis.render.scatterplot({ name: 'Event Data', 'tab': 'ScatterPlot' }, {
+    values: [birthdayValues, weddingValues, divorceValues, retirementValues],
+    series: [EVENT_TYPES[0], EVENT_TYPES[1], EVENT_TYPES[2], EVENT_TYPES[3]]
 },
-{
-    xLabel: 'Guests',
-    yLabel: 'Time (Hrs)'
-});
+    {
+        xLabel: 'Guests',
+        yLabel: 'Time (Hrs)'
+    });
 init();
 
 const knnClassifier = getKNNClassifier();
 /* BDAY DATA */
-var birthdayData: Array<Array<number>> = data.filter( (d: Party) => d.type == EVENT_TYPES[0]).map( (d: Party) => [ new Date(d.date).getHours(), d.guests]);
+var birthdayData: Array<Array<number>> = data.filter((d: Party) => d.type == EVENT_TYPES[0]).map((d: Party) => [new Date(d.date).getHours(), d.guests]);
 var bdayExample: tf.Tensor<any> = tf.tensor2d(birthdayData);
 /* WEDDING DATA */
-var weddingData: Array<Array<number>> = data.filter( (d: Party) => d.type == EVENT_TYPES[1]).map( (d: Party) => [ new Date(d.date).getHours(), d.guests]);
+var weddingData: Array<Array<number>> = data.filter((d: Party) => d.type == EVENT_TYPES[1]).map((d: Party) => [new Date(d.date).getHours(), d.guests]);
 var weddingExample: tf.Tensor<any> = tf.tensor2d(weddingData);
+/* WEDDING DATA */
+var divorceData: Array<Array<number>> = data.filter((d: Party) => d.type == EVENT_TYPES[2]).map((d: Party) => [new Date(d.date).getHours(), d.guests]);
+var divorceExample: tf.Tensor<any> = tf.tensor2d(divorceData);
+/* WEDDING DATA */
+var retirementData: Array<Array<number>> = data.filter((d: Party) => d.type == EVENT_TYPES[3]).map((d: Party) => [new Date(d.date).getHours(), d.guests]);
+var retirementExample: tf.Tensor<any> = tf.tensor2d(retirementData);
 //Training 
 // knnClassifier.addExample(bdayExample, EVENT_TYPES[0]);
 // knnClassifier.addExample(weddingExample, EVENT_TYPES[1]);
 knnClassifier.setClassifierDataset(
-    {['BIRTHDAY']: bdayExample,
-['WEDDING']: weddingExample}
-)
+    {
+        ['BIRTHDAY']: bdayExample,
+        ['WEDDING']: weddingExample,
+        ['DIVORCE']: divorceExample,
+        ['RETIREMENT']: retirementExample
+    }
+);
 //Predictions
 
 
@@ -75,20 +93,26 @@ const getPrediction = () => {
     let hours: string = document.getElementById('hours').value;
     //@ts-ignore
     let guests: string = document.getElementById('guests').value;
-    if(hours != null && guests != null){
+    //@ts-ignore
+    let kValue: string = document.getElementById('kValue').value ?? 1;
+    if (hours != null && guests != null) {
         let intHours = parseInt(hours);
         let intGuests = parseInt(guests);
-       let results = knnClassifier.predictClass(tf.tensor2d([[intGuests,intHours]]), 2);
+        let intKValue = parseInt(kValue);
+        let results = knnClassifier.predictClass(tf.tensor2d([[intGuests, intHours]]), intKValue);
 
         results.then(res => {
-            let { label } = res;
             console.log("RESULTS: ", res);
             let { confidences } = res;
-            let percentage = confidences[label] * 100;
-            document.getElementById('prediction-results').innerHTML = `RESULTS: ${label} with ${percentage}% confidence`;
+            let confSpread = Object.keys(confidences);
+            let resultStr = confSpread.map(conf => {
+                let percentage = confidences[conf] * 100;
+                return `${conf} with ${percentage}% confidence`;
+            })
+            document.getElementById('prediction-results').innerHTML = "RESULTS: \n" + resultStr.join('\n');
         });
     }
-    else{
+    else {
         document.getElementById('prediction-results').innerHTML = `RESULTS: Failed Inputs ${hours ?? 'null'} hours and ${guests ?? null} guests `;
     }
 }
